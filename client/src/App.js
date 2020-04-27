ed0185c15dd42c7d21818723e75bf67ef4b1a05e
Derek <48116904+SoLetsCode@users.noreply.github.com>
2020-04-27 16:53:29 -0700

    first commit with react framework

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+//styles
+import "./styles/app.css";
+
+//components
+import Test from "./components/Test";
+
+function App() {
+  return (
+    <div className="App">
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Test.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Test() {
+  return <div>TESTING WORKS</div>;
+}
