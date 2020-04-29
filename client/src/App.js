dffb96f2b8a53216d938882846e0d5f814075406
Derek <48116904+SoLetsCode@users.noreply.github.com>
2020-04-28 22:06:31 -0700

    added timer function to page and fires a NEXT event at the end. There is an issue that these events are fired multiple times rather than just once

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //styles
 import "./styles/app.scss";
-import logo from './assets/enso-logo.png';
+import logo from "./assets/enso-logo.png";
 // icons
-import camera from './assets/camera.png';
-import microphone from './assets/microphone.png';
-import screenShare from './assets/screen-share.png';
-import options from './assets/options.png';
-import phone from './assets/phone.png';
-import add from './assets/add.png';
-import previous from './assets/previous.png';
-import pause from './assets/pause.png';
-import next from './assets/next.png';
+import camera from "./assets/camera.png";
+import microphone from "./assets/microphone.png";
+import screenShare from "./assets/screen-share.png";
+import options from "./assets/options.png";
+import phone from "./assets/phone.png";
+import add from "./assets/add.png";
+import previous from "./assets/previous.png";
+import pause from "./assets/pause.png";
+import next from "./assets/next.png";
 // users
-import user1 from './assets/lea.png';
-import user2 from './assets/benett.png';
-import user3 from './assets/tina.png';
+import user1 from "./assets/lea.png";
+import user2 from "./assets/benett.png";
+import user3 from "./assets/tina.png";
 // poses
-import pose1 from './assets/yoga.png';
+import pose1 from "./assets/yoga.png";
+import pose2 from "./assets/yoga2.png";
+import pose3 from "./assets/yoga3.png";
+
+//socket.io
+import io from "socket.io-client";
 
 //components
-// import Timer from "./components/Timer";
-import VidBtn from './components/VidBtn';
-import User from './components/User'
+import VidBtn from "./components/VidBtn";
+import User from "./components/User";
+
+const socket = io("http://localhost:5001");
 
 function App() {
+  const [timer, setTimer] = useState(0);
+
   const dummy = {
     users: [
       {
-        name: 'Lea',
-        img: user1
+        name: "Lea",
+        img: user1,
       },
       {
-        name: 'Benett',
-        img: user2
+        name: "Benett",
+        img: user2,
       },
       {
-        name: 'Tina',
-        img: user3
-      }
-    ]
-  }
+        name: "Tina",
+        img: user3,
+      },
+    ],
+  };
+
+  useEffect(() => {
+    socket.on("timer", (time) => {
+      setTimer(time);
+    });
+  });
+
+  useEffect(() => {
+    socket.on("next", (msg) => {
+      console.log("next");
+    });
+  });
 
   return (
     <div className="App">
@@ -54,18 +74,14 @@ function App() {
 
       {/* main content container */}
       <div className="main">
-
         <div className="vid-chat">
-
           <div className="vid-chat__controller">
-            <div className="vid-chat__time">
-              3:10:12
-            </div>
+            <div className="vid-chat__time">3:10:12</div>
             <VidBtn icon={camera} alt="toggle video" />
             <VidBtn icon={microphone} alt="toggle microphone" />
             <VidBtn icon={screenShare} alt="screen share" />
             <VidBtn icon={options} alt="options" />
-            <VidBtn icon={phone} alt="end call" background='red' />
+            <VidBtn icon={phone} alt="end call" background="red" />
           </div>
 
           <div className="vid-chat__participants">
@@ -78,44 +94,56 @@ function App() {
             </div>
             <div className="vid-chat__users">
               {dummy.users.map((user, index) => {
-                return <User key={user.name + index} user={user} />
+                return <User key={user.name + index} user={user} />;
               })}
             </div>
           </div>
-
         </div>
 
         <div className="display">
           <div className="display__container">
             <div className="display__header">
               <div className="display__info">
-                <h2 className="display__stretch-info">Sun Salutation | Round 1/4</h2>
+                <h2 className="display__stretch-info">
+                  Sun Salutation | Round 1/4
+                </h2>
                 <h1 className="display__pose-info">Pose 3/9</h1>
               </div>
               <div className="display__timer">
                 {/* TODO timer will go in here */}
-              06:57
-            </div>
+                00:{timer < 10 ? "0" + timer : timer}
+              </div>
             </div>
             <div className="display__pose">
-              <img className="display__pose-img" src={pose1} alt="current pose" />
+              <img
+                className="display__pose-img"
+                src={pose1}
+                alt="current pose"
+              />
               <p className="display__pose-name">Standing Half Forward Fold</p>
             </div>
           </div>
 
           <div className="display__controller">
             <button className="display__btn">
-              <img className="display__icon" src={previous} alt="previous pose" />
+              <img
+                className="display__icon"
+                src={previous}
+                alt="previous pose"
+              />
             </button>
             <button className="display__btn">
-              <img className="display__icon display__icon--large" src={pause} alt="previous pose" />
+              <img
+                className="display__icon display__icon--large"
+                src={pause}
+                alt="previous pose"
+              />
             </button>
             <button className="display__btn">
               <img className="display__icon" src={next} alt="previous pose" />
             </button>
           </div>
         </div>
-
       </div>
     </div>
   );
